8088671a94d89054e24a7661a3942aa4c2d3ade9
LuminiferousGiraffe <andreas.quist99@gmail.com>
2025-01-23 17:41:19 -0500

    temp commit

---
+++ Build/makeLove.bat
@@ -0,0 +1,6 @@
+cd C:\Users\Andreas\GitHub\Phyz\Build
+powershell -command "Compress-Archive -Path '.\BuildStaging\*' -DestinationPath '.\Phyz.zip'"
+cmd /c "ren Phyz.zip Phyz.love"
+del "./Phyz.zip"
+copy /b "C:\Program Files (x86)\LOVE\love.exe"+".\Phyz.love" "..\bin\Phyz.exe"
+del "./Phyz.love"
